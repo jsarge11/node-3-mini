aef0dad588173a664948a1673dac7019acd4b61e
Jay Sargent <sargentjay11@gmail.com>
2018-04-12 10:00:39 -0600

    completed

--- server/index.js
+++ server/index.js
@@ -1,16 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+const createInitialSession = require('./middleware/session')
+const session = require('express-session')
+const filter = require('./middleware/filter')
+require('dotenv').config()
 
 const app = express();
 
 app.use( bodyParser.json() );
-app.use( express.static( `${__dirname}/../build` ) );
+app.use( express.static( `../build` ) );
 
+app.use(session({ 
+ secret: process.env.SESSION_SECRET,
+ resave: false,
+ saveUninitialized: true,
+ cookie: {
+  maxAge: 10000
+ }
+}))
+
+// how is this capturing req, res, and next
+app.use ( ( req, res, next )  => createInitialSession( req, res, next) );
+app.use ( ( req, res, next ) => {
+ if (req.method === "PUT" || req.method === "POST") {
+  filter(req, res, next)
+ }
+ else {
+  next();
+ }
+})
+
+
+app.get("/api/messages/history", mc.history);
 app.post( "/api/messages", mc.create );
 app.get( "/api/messages", mc.read );
 app.put( "/api/messages", mc.update );
 app.delete( "/api/messages", mc.delete );
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
---
+++ server/middleware/filter.js
@@ -0,0 +1,10 @@
+let notAllowed = ["bitch", "slut"];
+
+module.exports = function(req, res, next) {
+  while ( notAllowed.find( word => req.body.text.includes(word) ) ) {
+   const badWord = notAllowed.find( word => req.body.text.includes(word) );
+   req.body.text = req.body.text.replace( badWord, '*'.repeat( badWord.length ) );
+  }
+
+next();
+}
